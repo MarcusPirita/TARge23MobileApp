a5e83cbb8119046cf3b06ce5eee6293cc7e0f57b
Opilane TTHK <opilane@tthk.ee>
2024-09-30 14:15:32 +0300

    30.09.24

--- MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
+++ MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
@@ -1,25 +1,16 @@
-﻿namespace MauiCRUD
+﻿using MauiCRUD.ViewModels;
+
+namespace MauiCRUD
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
-        public MainPage()
+        private readonly ProductsViewModel
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
