e5b6b5bd207e82c7806bcfbee3e3cf2f0ba01593
Opilane TTHK <opilane@tthk.ee>
2024-10-01 16:12:54 +0300

    fix

--- MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
+++ MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
@@ -5,12 +5,19 @@ namespace MauiCRUD
     public partial class MainPage : ContentPage
     {
 
-        private readonly ProductsViewModel
+        private readonly ProductsViewModel _productsViewModel;
+        
+        public MainPage(ProductsViewModel productsViewModel)
         {
             InitializeComponent();
+            BindingContext = productsViewModel;
+            _productsViewModel = productsViewModel;
         }
 
-
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await _productsViewModel.LoadProductsAsync();
+        }
     }
-
 }
